9275a3d37676abf874e3afd5a54f52725df4824a
Boobala krishnan <boobalakrishnanv@gmail.com>
2023-06-20 21:07:23 +0530

    removing address and employee service

--- address-service/src/main/java/com/microservice/addressapp/response/AddressResponse.java
+++
@@ -1,9 +0,0 @@
-package com.microservice.addressapp.response;
-import lombok.Data;
-
-@Data
-public class AddressResponse {
-    private int id;
-    private String street;
-    private String district;
-}
--- employee-service/src/main/java/com/microservice/employeeapp/response/AddressResponse.java
+++
@@ -1,9 +0,0 @@
-package com.microservice.employeeapp.response;
-import lombok.Data;
-
-@Data
-public class AddressResponse {
-    private int id;
-    private String street;
-    private String district;
-}
